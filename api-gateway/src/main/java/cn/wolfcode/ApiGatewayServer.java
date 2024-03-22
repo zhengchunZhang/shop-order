bb1d30ab1113a849251f85e7f78d6560fc6766a1
zhangzhengchun <1437237338@qq.com>
2024-03-22 22:47:24 +0800

    GateWay快速入门

---
+++ api-gateway/src/main/java/cn/wolfcode/ApiGatewayServer.java
@@ -0,0 +1,11 @@
+package cn.wolfcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayServer {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayServer.class,args);
+    }
+}
